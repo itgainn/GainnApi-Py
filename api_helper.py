e2de0d01cdd2f93e2d769c41cfd53440bf271b63
Nikhesh <nikhesh.p@kambala.co.in>
2024-08-14 12:12:49 +0530

    UAT url updated

--- api_helper.py
+++ api_helper.py
@@ -39,7 +39,7 @@ def get_time(time_string):
 
 class NorenApiPy(NorenApi):
     def __init__(self):
-        NorenApi.__init__(self, host='http://rama.kambala.co.in:6002/NorenWClient/', websocket='ws://rama.kambala.co.in:5552/NorenWS/')
+        NorenApi.__init__(self, host='http://uatexe.mygainn.com/NorenWClientTP/', websocket='wss://uatexe.mygainn.com/NorenWS/')
         global api
         api = self
     def place_basket(self, orders):
